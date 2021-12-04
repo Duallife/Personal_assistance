0dc85831681357685abda6f24f49c8574e29b9d7
Duallife <85776119+Duallife@users.noreply.github.com>
2021-12-04 21:23:35 +0800

    Add files via upload

--- ISDN2001/src/main.cpp
+++ ISDN2001/src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <lvgl.h>
 #include <TFT_eSPI.h>
+
+#include <WiFi.h>
+#include <WifiUdp.h>
+#include <NTPClient.h>
+// #include <cstring>
 /*If you want to use the LVGL examples,
   make sure to install the lv_examples Arduino library
   and uncomment the following line.
@@ -15,6 +20,13 @@ static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf[ screenWidth * 10 ];
 
 TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight); /* TFT instance */
+
+//clock
+const char* ssid       = "Xie6_Wi-Fi5";
+const char* password   = "55375157";
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000 );
 
 static void event_handler(lv_event_t * e);
 void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data );
@@ -34,6 +46,10 @@ static void sw_event_cb(lv_event_t * e);
 // }
 
 // lv_obj_t * screen = lv_obj_create(NULL);
+void time_task(lv_timer_t * task);
+static void load_scr1();
+static void time_cb(lv_event_t * e);
+int test = 0;
 
 void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
 {
@@ -85,6 +101,14 @@ void setup()
 
     // Serial.println( LVGL_Arduino );
     // Serial.println( "I am LVGL_Arduino" );
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println(" CONNECTED");
+
+    timeClient.begin();
 
     lv_init();
 
@@ -121,7 +145,21 @@ void setup()
     // lv_label_set_text( label, LVGL_Arduino.c_str() );
     // lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
 
+    load_scr1();
 
+    Serial.println( "Setup done" );
+}
+
+void loop()
+{
+    lv_timer_handler(); /* let the GUI do its work */
+    delay( 5 );
+    test++;
+}
+
+static void load_scr1(){
+    lv_obj_t * scr1 = lv_obj_create(NULL);
+    lv_scr_load(scr1);
 
     lv_obj_t * btn1 = lv_btn_create(lv_scr_act());
     lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
@@ -144,38 +182,67 @@ void setup()
     lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
 
 /////////////////////////
-    lv_obj_t * label1 = lv_label_create(lv_scr_act());
-    lv_label_set_text(label1, "Hello animations!");
-    lv_obj_set_pos(label1, 55, 20);
+    lv_obj_t * obj = lv_obj_create(lv_scr_act());
+    lv_obj_set_style_bg_color(obj, lv_palette_main(LV_PALETTE_RED), 0);
+    lv_obj_set_style_radius(obj, LV_RADIUS_CIRCLE, 0);
+    lv_obj_set_size(obj, 10, 10);
+    lv_obj_set_pos(obj,10,10);
 
 
     lv_obj_t * sw = lv_switch_create(lv_scr_act());
     lv_obj_center(sw);
     lv_obj_set_pos(sw, 0, -50);
     lv_obj_add_state(sw, LV_STATE_CHECKED);
-    lv_obj_add_event_cb(sw, sw_event_cb, LV_EVENT_VALUE_CHANGED, label1);
+    lv_obj_add_event_cb(sw, sw_event_cb, LV_EVENT_VALUE_CHANGED, obj);
 
-    
+//////////////////////////////
+    // lv_obj_t * time = lv_label_create(lv_scr_act());
+    // lv_obj_center(time);
+    // lv_obj_set_pos(time,0,100);
+    // lv_label_set_text(time, "Finding");
+    lv_obj_t * timebtn = lv_btn_create(lv_scr_act());
+    lv_obj_center(timebtn);
+    lv_obj_set_pos(timebtn, 0, 100);
+    lv_obj_add_event_cb(timebtn, time_cb, LV_EVENT_ALL, NULL);
 
 
-    Serial.println( "Setup done" );
 }
-
-void loop()
-{
-    lv_timer_handler(); /* let the GUI do its work */
-    delay( 5 );
-}
-
-
 //support functions
+lv_obj_t * scr2;
+
+void load_scr2(){
+    scr2 = lv_obj_create(NULL);
+    lv_scr_load(scr2);
+
+    lv_obj_t * time = lv_label_create(scr2);
+    lv_timer_t * timer = lv_timer_create(time_task, 1000, time);
+    lv_timer_ready(timer);
+}
+
+void time_task(lv_timer_t * task){
+    
+    lv_obj_t * time = (lv_obj_t*)task->user_data;
+    timeClient.update();
+    lv_obj_center(time);
+    lv_obj_set_pos(time,0,100);
+    lv_label_set_text(time, timeClient.getFormattedTime().c_str());
+}
+
+static void time_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+
+    if(code == LV_EVENT_CLICKED) {
+        load_scr2();
+    }
+}
+
 static void event_handler(lv_event_t * e)
 {
     lv_event_code_t code = lv_event_get_code(e);
 
     if(code == LV_EVENT_CLICKED) {
         Serial.println("ihi1");
-        // lv_scr_load(screen);
     }
 }
 
@@ -192,28 +259,41 @@ static void anim_x_cb(void * var, int32_t v)
 {
     lv_obj_set_x((lv_obj_t *)var, v);
 }
+
+static void anim_size_cb(void * var, int32_t v)
+{
+    lv_obj_set_size((lv_obj_t *)var, v, v);
+}
+
 static void sw_event_cb(lv_event_t * e)
 {
     lv_obj_t * sw = lv_event_get_target(e);
-    lv_obj_t * label1 = (lv_obj_t *)lv_event_get_user_data(e);
+    lv_obj_t * obj = (lv_obj_t *)lv_event_get_user_data(e);
 
     if(lv_obj_has_state(sw, LV_STATE_CHECKED)) {
         lv_anim_t a;
         lv_anim_init(&a);
-        lv_anim_set_var(&a, label1);
-        lv_anim_set_values(&a, lv_obj_get_x(label1), 55);
+        lv_anim_set_var(&a, obj);
         lv_anim_set_time(&a, 1000);
-        lv_anim_set_exec_cb(&a, anim_x_cb);
-        lv_anim_set_path_cb(&a, lv_anim_path_overshoot);
+        lv_anim_set_path_cb(&a, lv_anim_path_ease_in_out);
+        lv_anim_set_exec_cb(&a, anim_size_cb);
+        lv_anim_set_values(&a, 50, 10);
         lv_anim_start(&a);
+        lv_anim_set_exec_cb(&a, anim_x_cb);
+        lv_anim_set_values(&a, 50, 10);
+        lv_anim_start(&a);
+
     } else {
         lv_anim_t a;
         lv_anim_init(&a);
-        lv_anim_set_var(&a, label1);
-        lv_anim_set_values(&a, lv_obj_get_x(label1), -lv_obj_get_width(label1));
+        lv_anim_set_var(&a, obj);
         lv_anim_set_time(&a, 1000);
+        lv_anim_set_path_cb(&a, lv_anim_path_ease_in_out);
         lv_anim_set_exec_cb(&a, anim_x_cb);
-        lv_anim_set_path_cb(&a, lv_anim_path_ease_in);
+        lv_anim_set_values(&a, 10, 50);
+        lv_anim_start(&a);
+        lv_anim_set_exec_cb(&a, anim_size_cb);
+        lv_anim_set_values(&a, 10, 50);
         lv_anim_start(&a);
     }
 
